22a0130c875a91f343b50385b7e4049f4abb8287
kavnthir <ksbkthirukonda@gmail.com>
2021-07-09 17:59:34 -0400

    fonts and css stuff

--- local/app_local.js
+++ local/app_local.js
@@ -51,7 +51,7 @@ let displayUserSelect = function(users) {
         nameDiv.append(nameSpan);
 
         let checkboxDiv = $("<div>", { class: "checkbox-wrapper" });
-        let checkboxInput = $("<input>", { type: "checkbox" });
+        let checkboxInput = $("<input>", { type: "checkbox", class: "checkbox" });
         checkboxDiv.append(checkboxInput);
 
         let rowDiv = $("<div>", { class: "row-wrapper" });
