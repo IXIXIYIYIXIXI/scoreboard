476fde9bcc1029793cbad2768acaf36671cf9a59
IXIXIYIYIXIXI <ixixiyiyixixi@gmail.com>
2021-07-21 22:05:23 -0400

    fixed back button

--- local/buttons_local.js
+++ local/buttons_local.js
@@ -11,22 +11,37 @@ function deselectAllButtonPressed() {
 }
 
 function startButtonPressed() {
-    let players = [];
+    let playersToAdd = [];
     $("#userSelect").children().each(function(_, rowDiv) {
-        if ($(rowDiv).find(".checkbox").is(":checked")) {
-            players.push($(rowDiv).data("user"));
+        let inScoreboard = false;
+        let scoreboardObject = null;
+        $("#scoreboard").children().each(function(_, scoreboardDiv) {
+            let isEqual = function(row, sb) {
+                let pfp = $(row).find(".pfp").attr("src") === $(sb).find(".pfp").attr("src");
+                let name = $(row).find(".name").text() === $(sb).find(".name").text();
+                let color = $(row).find(".name").css("color") === $(sb).find(".name").css("color");
+                return pfp && name && color;
+            };
+            if (isEqual(rowDiv, scoreboardDiv)) {
+                inScoreboard = true;
+                scoreboardObject = scoreboardDiv;
+            }
+        });
+        if (inScoreboard && !$(rowDiv).find(".checkbox").is(":checked")) {
+            $(scoreboardObject).remove();
+        } else if (!inScoreboard && $(rowDiv).find(".checkbox").is(":checked")) {
+            playersToAdd.push($(rowDiv).data("user"));
         }
     });
-    if (players.length > 0) {
-        startGame(players);
+    if (playersToAdd.length + $("#scoreboard").children().length > 0) {
+        startGame(playersToAdd);
     }
 }
 
 function backButtonPressed() {
-    if($("#perigame").hasClass("hidden")){
-        console.log("test");
+    if ($("#perigame").hasClass("hidden")) {
         window.location.href = "../index.html";
-    }else{
+    } else {
         $("#pregame").removeClass("hidden");
         $("#perigame").addClass("hidden");
     }
